2a07a948db848f8b89a3debb71c1ffa022c0beaa
saaya-code <boualimohamed74@gmail.com>
2022-11-10 13:37:19 +0100

    Added map not complete

--- DevFest/src/components/Map.jsx
+++ DevFest/src/components/Map.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import { useEffect } from 'react';
+import { Wrapper } from '@googlemaps/react-wrapper';
 export default function Chat() {
+
   useEffect(()=>{
-    document.title = "City Map"
+    navigator.geolocation.getCurrentPosition((pos)=>{
+      const ifameData=document.getElementById("iframeID")
+      document.title = "City Map"
+      const lat = pos.coords.latitude|| 1.305385;
+      const lon = pos.coords.longitude || 30.923029;
+      ifameData.src=`https://maps.google.com/maps?q=${lat},${lon}&hl=es;&output=embed`
+    })
+
+
 });
+
   return (
-    <div>Chat</div>
+    <div id="map">
+      <iframe height="500px" width="500px" id="iframeID"></iframe>
+    </div>
   )
 }
